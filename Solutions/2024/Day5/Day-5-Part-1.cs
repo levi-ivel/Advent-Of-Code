0dea28f54d64d61a787c959015d3e49e88049bae
levi <levi.kingma@gmail.com>
2024-12-05 12:16:47 +0100

    Feat: solved day 5 part 1 Feat: solved day 5 part 2

--- Solutions/2024/Day5/Day-5-Part-1.cs
+++ Solutions/2024/Day5/Day-5-Part-1.cs
@@ -1,11 +1,10 @@
 // To see puzzle description, see README
 
-// Time complexity: O(n * m), where n is the amount of rows and m the amount of columns
-// Execution time: varies between 3 and 34 miliseconds
+// Time complexity: O(n * m), where n is the amount of ordering rules and m the amount of updates
+// Execution time: 14 miliseconds
 
 using System;
 using System.Diagnostics;
-using System.Text.RegularExpressions;
 
 class Day5Part1_2024
 {
@@ -13,13 +12,80 @@ class Day5Part1_2024
     {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-        
 
-        // Define, read and change input into a grid
-        string day4Input = "Solutions/2024/Day4/Inputs/Day4Input.txt";
+        // Define and read input
+        string day5Input = "Solutions/2024/Day5/Inputs/Day5Input.txt";
+        string[] lines = File.ReadAllLines(day5Input);
 
-        string[] lines = File.ReadAllLines(day4Input);
+        // Define lists for the left numbers, right numbers an the update numbers
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+        List<List<int>> updateLists = new List<List<int>>();
 
+        // Parse into 2 lists (number before the | goes left, after goes right etc.), 
+        // stopping when an empty line is encountered
+        bool parsingRules = false;
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("|"))
+            {
+                string[] splitLine = line.Split('|');
+                leftList.Add(int.Parse(splitLine[0].Trim()));
+                rightList.Add(int.Parse(splitLine[1].Trim()));
+            }
+            else if (!string.IsNullOrWhiteSpace(line))
+            {
+                parsingRules = true;
+                updateLists.Add(line.Split(',').Select(int.Parse).ToList());
+            }
+        }
+
+        // Define total counter
+        int totalCounter = 0;
+
+        if (parsingRules)
+        {
+            for (int updateIndex = 0; updateIndex < updateLists.Count; updateIndex++)
+            {
+                // Define the list of an update line, and if the list passes the rules
+                var updateList = updateLists[updateIndex];
+                bool rulePassed = true;
+                
+                // Go trough each pair, a pair is the same index from the left and right list 
+                for (int i = 0; i < leftList.Count; i++)
+                {
+                    int leftValue = leftList[i];
+                    int rightValue = rightList[i];
+                    int leftIndex = updateList.IndexOf(leftValue);
+                    int rightIndex = updateList.IndexOf(rightValue);
+
+                    // If one of the integers from the pair is not present, skip pair
+                    if (leftIndex == -1 || rightIndex == -1)
+                    {
+                        continue;
+                    }
+                    
+                    // If the right number of the pair appears earlier then the left, fail the list
+                    if (leftIndex >= rightIndex)
+                    {
+                        rulePassed = false;
+                        break;
+                    }
+                }
+
+                // If the list was not failed, add the value of the middle index's integer to the couner
+                if (rulePassed)
+                {
+                    int middleIndex = updateList.Count / 2;
+                    int middleValue = updateList[middleIndex];
+                    totalCounter += middleValue;
+                }
+            }
+        }
+
+        // Display total counter
+        Console.WriteLine($"\nTotal counter: {totalCounter}");
 
         stopwatch.Stop();
         Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} milliseconds.");
--- Solutions/2024/Day5/Day-5-Part-2.cs
+++ Solutions/2024/Day5/Day-5-Part-2.cs
@@ -1,7 +1,7 @@
 // To see puzzle description, see README
 
-// Time complexity: O(n * m), where n is the amount of rows and m the amount of columns
-// Execution time: varies between 3 and 34 miliseconds
+// Time complexity: O(n * m ^ 2), where n is the amount of ordering rules and m the amount of updates
+// Execution time: 17 miliseconds
 
 using System;
 using System.Diagnostics;
@@ -12,13 +12,92 @@ class Day5Part2_2024
     {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-        
 
-        // Define, read and change input into a grid
-        string day4Input = "Solutions/2024/Day4/Inputs/Day4Input.txt";
+        // Define and read input
+        string day5Input = "Solutions/2024/Day5/Inputs/Day5Input.txt";
+        string[] lines = File.ReadAllLines(day5Input);
 
-        string[] lines = File.ReadAllLines(day4Input);
+        // Define lists for the left numbers, right numbers an the update numbers
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+        List<List<int>> updateLists = new List<List<int>>();
 
+        // Parse into 2 lists (number before the | goes left, after goes right etc.), 
+        // stopping when an empty line is encountered
+        bool parsingRules = false;
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("|"))
+            {
+                string[] splitLine = line.Split('|');
+                leftList.Add(int.Parse(splitLine[0].Trim()));
+                rightList.Add(int.Parse(splitLine[1].Trim()));
+            }
+            else if (!string.IsNullOrWhiteSpace(line))
+            {
+                parsingRules = true;
+                updateLists.Add(line.Split(',').Select(int.Parse).ToList());
+            }
+        }
+
+        // Define total counter
+        int totalCounter = 0;
+
+        if (parsingRules)
+        {
+            for (int ruleIndex = 0; ruleIndex < updateLists.Count; ruleIndex++)
+            {
+                // Define the list of an update line, if the list passes the rules 
+                // and if the list has been modified
+                var ruleList = updateLists[ruleIndex];
+                bool rulePassed = false;
+                bool isListModified = false;  
+
+                while (!rulePassed)
+                {
+                    rulePassed = true;  
+
+                    // Go trough each pair, a pair is the same index from the left and right list
+                    for (int i = 0; i < leftList.Count; i++)
+                    {
+                        int leftValue = leftList[i];
+                        int rightValue = rightList[i];
+                        int leftIndex = ruleList.IndexOf(leftValue);
+                        int rightIndex = ruleList.IndexOf(rightValue);
+
+                        // If one of the integers from the pair is not present, skip pai
+                        if (leftIndex == -1 || rightIndex == -1)
+                        {
+                            continue;
+                        }
+
+                        // If the right number of the pair appears earlier then the left, fail the list
+                        // and modify it by moving the left integer to a position before the right integer
+                        if (leftIndex >= rightIndex)
+                        {
+                            rulePassed = false;
+
+                            ruleList.RemoveAt(leftIndex);
+                            ruleList.Insert(rightIndex, leftValue);
+
+                            isListModified = true;
+                        }
+                    }
+                }
+
+                // If the list was not failed, add the value of the middle index's integer to the couner
+                if (isListModified)
+                {
+                    int middleIndex = ruleList.Count / 2;
+                    int middleValue = ruleList[middleIndex];
+                    totalCounter += middleValue;
+                }
+            }
+        }
+
+        // Display total counter
+        Console.WriteLine($"\nTotal counter: {totalCounter}");
 
         stopwatch.Stop();
         Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} milliseconds.");
